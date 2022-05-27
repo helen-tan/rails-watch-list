51fbfb27a9ea7c0837a8665862943e832e74febe
helen tan <helentanmz0909@gmail.com>
2022-05-27 14:01:49 +0800

    Created lists routes, ListsController, list views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # User can see all the lists
+  get "lists", to: "lists#index"
+
+  # User can create a new list
+  get "lists/new", to: "lists#new", as: :new_list
+  post "lists", to: "lists#create"
+
+  # User can see the details of a given list and its name
+  get "lists/:id", to: "lists#show", as: :list
 end
